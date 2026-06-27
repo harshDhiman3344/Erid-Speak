128c95c8a241d94e916356731a30950f6edb18cc
HarshDhiman <142768740+harshDhiman3344@users.noreply.github.com>
2026-06-27 23:39:23 +0530

    committtt

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1>ERIDIAN COMMUNICATION INTERFACE</h1>
+      <p>Awaiting human input...</p>
+    </main>
+  )
+}
+
+export default App
